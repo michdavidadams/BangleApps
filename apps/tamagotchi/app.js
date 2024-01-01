617401a0e2344dd8dcdb547a6b73d11028c84d1f
Michael Adams <me@michdavidadams.com>
2023-12-31 19:57:00 -0500

    Update app.js

--- apps/tamagotchi/app.js
+++ apps/tamagotchi/app.js
@@ -1,35 +1,81 @@
-// Clock to mimic the 1996 Tamagotchi
-const black = "#000000";
-const yellow = "#ffff00";
-
 { // must be inside our own scope here so that when we are unloaded everything disappears
   // we also define functions using 'let fn = function() {..}' for the same reason. function decls are global
 let drawTimeout;
 
 // Actually draw the watch face
 let draw = function() {
-  var x = g.getWidth();
-  var y = g.getHeight();
+  let x = g.getWidth();
+  let y = g.getHeight();
+  let eggInner = 10; // max size of inner shell
   g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
   
-  // Draw background
-  g.drawImage({
-  width : 256, height : 140, bpp : 3,
-  transparent : 0,
-  buffer : require("heatshrink").decompress(atob("tu27YCLtwHGrYGD3YWG2ojNAQntBZImHHBIXKL/5fuFg3WBwwTFLiBfDOhCSHRh47VL/5ffHAIjLNwjyKC5aDHL5wvHI4Zf/L+bXDBxhHHARZTFF5ACPGQSYDL/5f1txQOFIKDUI4229pfSARRf/L+IUPdIRxOKZQdDKyVbEw4IFL/5fa2obPHYhQKtzmRKZYpEEYoUOAQpf/L777PHY5fOeQ5NPTxYUVL/5ffHBQjLO5ARRL5abMF44ONL/5ffZxW7FZhfPFI4jRMSJTIL/5fifBAoPC4ftChwRNrZfTEBJf/L8wmF3YrScwJfPFgVuQBgCQI6Rf/L75HUAS6MDMQxfTPpJf/L9QCYIITvOKY6VEGaRcqL/7yL9pfUIIxiLL44CCtwHEGZpHUL/5faEYREFFgoCOHCJxGAQgsMO4qJLL/5fkCINbbS4CBDRBQJBxwCPMoKnOL/5ffBwQMJbpzyFARiDCR7Vt2oGEMRZf/L74UIAQoaML6KAHRiQXII6Bf/L75HNepJ3LL5w1FIhgCDrYMLDpJf/L77XKIhb/RGRZ9FMRinMDphf/L74aPMQbsMAQoNLC5XtOJYyVL/5ffJqJiBFiBHGL6ACDtwRQL/5ftbp4vCOiBfLR6ACBIJxf/L9wyNJQwyM3YfK6xKRGQR0SL/5fqJpRKKBJFuG5ZfRGQoXPU4xf/L8odIJRj1HRKp9QCihf/L8wdGahpiHL7qPRI6Zf/L6LCP6wDB3Z0PQywCLSRpECAwRHEL/5ffDR4CW2pEMrZfeQAgIHL/5ff7duDRg7Ba4gCNKBBHEfwoCLGSRoJL/5ffDpg7EEx5QOF4JFOUJoCD2qJFL/5flKBTvUJqgRLR6gmLL/5ffKAwXEGR4UKL5ZiK9pfOC5Zf/L8xQEI5o1G2r8PPRQyFTYqPTL/5frEwNbI51uAgYUPO5ACXBhh6BL/5foJoW1FqFudhQyIEp/WOJQvRL/5fmEYxiMJQh0PL6CbFBZBf/L9w+FEBAjLCJBiLFJYCEraeJ6y5SL/5ffCggdEMR27FBo7HIB4XHO4SGLL/5fpCaQCVHwiJPKZXtGyhf/L8Nbtu7CyNbd4QuRL6IJJIgJrKL/5fpCgo1UFyaGPU5QOML/5fobpAKIcxIUKDRFuBxopOL/5f4cAwCB6wUTAQ4GE9oONAQZ0JHxosCL/5focYrXPfA4CErZNNBwyGNChIpFL/5ff2oOLbp77X9oOSAQ5QKC4Zf/L77RKBwZfRfxD4Y3YuNO44RHL/5ffHArjHepICLDoSGICIxfINYQuOLhZf/L8RTCNyACPtx3O3YECOIwuTARpf/L75uQCqJNINwwHE2oLD9o+SPoZf/L9dbF4oCIL6BEDJohuIFI5KOTY5f/L9Y4DEywCFPo9bCIz+PGqYjKL/5feGovtFhwMKDRKJDNxImFDpB9HGp5f/L7oyFAQTvQF4xNLCgiJLEYYgFHhwXIL/5ffBxwCM6wDB3Y3REBoGDOIIIGARyJDL/5foJTwCHExe1BI4IIARx3ML/5ffZwhKMCiIvEEBojEI5hQJBxxf/L7w4JAQNbBJFuOJ4CB3YjNBwYvJHBe1L/5ftJQoUOfwYHF6yJNBw41WEZhf/L8rpCEyIpGAQL7XA49bSqYUNL/5fdCIQjOJpLpPFgJHNQCQUQL/5fcEwxfUJpQRJ2oIF3YTKQZR0QL/5fdZxA1GARxEBC55WCJog4ITxh0KL/5ffEwntEZVbL6IXRKwgRDFZ50RL/5ffASAPPdg5xQtu1TBgmIVphf/L8TOOGoIPMfZwCEKyQCVGQJf/L8A1C3YXOrYMJDRoUPTwgCOTwJ9JVQRf/L77vUC44+PC4tuBxQLIQZaPPL/5fgcyNbCiYXPAYe7ECR0HBYZf/L74"))
-}, 0, 20);
+  // Draw egg shell
+  //g.setColor(g.theme.bg);
+  //g.setColor("#ff0000");
+  //g.fillRect(0, 0, x, y); //outer shell
+  //g.setColor(g.theme.bg2);
+  g.setColor(g.theme.bg2);
+  g.fillRect(0, 0, x, y); // inner shell
+  // Draw egg cracks
+  g.setColor(0, 0, 0);
+  // Top
+  g.fillPoly([
+    0,0,
+    x*0.1,9,
+    x*0.2,0,
+    x*0.4,7,
+    x*0.5,0,
+    x*0.6,8,
+    x*0.8,0,
+    x*0.9,4,
+    x,0
+  ]);
+  // Right
+  g.fillPoly([
+    x,0,
+    x-9,y*0.1,
+    x,y*0.2,
+    x-6,y*0.4,
+    x,y*0.6,
+    x-8,y*0.8,
+    x,y
+  ]);
+  // Bottom
+  g.fillPoly([
+    0,y,
+    x*0.1,y-9,
+    x*0.2,y,
+    x*0.4,y-7,
+    x*0.5,y,
+    x*0.6,y-8,
+    x*0.8,y,
+    x*0.9,y-4,
+    x,y
+  ]);
+  // Left
+    g.fillPoly([
+    0,0,
+    9,y*0.1,
+    0,y*0.2,
+    6,y*0.4,
+    0,y*0.5,
+    8,y*0.6,
+    0,y*0.7,
+    9,y*0.8,
+    0,y
+   ]);
+  
+  // Main background
+  g.setColor(1, 1, 1);
+  g.fillRect(10, 10, x-10, y-10);
 
   // Draw tama
+  g.setColor(0, 0, 0);
   g.drawImage({
-  width : 39, height : 45, bpp : 2,
-  transparent : 0,
-  buffer : require("heatshrink").decompress(atob("gEP/4ECgf/4AEBn/8Bw//+AECBwf/AAIICAgPAEAP/BwwgBDoQOEC4QAEBwIIGEwgAFEwJQCAAYmCBAsDEwJfDMQYmBBYgaDBYgOEBYYOEF4yPGAAiPEIginEBwanFWw53CNYZ0cCgpfCCgwRCPohBCAoRiFYQhEDKAZiEAoTCXUgoCEBAodGR4YdELgxKBBwpuBJQKhEQYRuBGoiVHQYo+GLhDOHMQ5cGAAYA=="))
-}, (x/2)-17, (y/2)-23);
-  
-  // Draw bars
-  g.setColor(yellow);
-  g.fillRect(0, 0, x, 20);
-  g.fillRect(0, y-20, x, y);
+  width : 70, height : 81, bpp : 3,
+  transparent : 1,
+  buffer : require("heatshrink").decompress(atob("kmSpMkwAHChIIDAQgVDiQLGkALCgQLGAQNABoUEBY1IEwYyHkmQBgUBEw5WHAQoNDBY5oXHwY1KBowpDQAYADLgp0EKAwLLOgxQHBZZ0FAAa5DBZZcNNEQANNBYANNBYANNBYAOND7LFARZiBNArLFARZiBNArLFARZiCApACONH8kNwxcBgRoPNw4HBNB9ANwxcBNB+QNwxcBhJohdKRWGdKZWFdKZWFAALpRX4xojBYZcJNBYLDLhJoLBYZcKBZxcONGMCCIS5ROgQFIdKBKDKBLpLKwYFFARpiFNDAXFDpZEBMQqAFAoMJDRALDMQdAQAgFBX4IaHBYZiDyCAEAoMAJpALDNBZcCDQ4LDNBabJNA6MDQBZoDAAbRFAAKAJNAgADa4qAKNAgADWYYHDJpRoCAAZoTIghoGXIgCMaIgFGARxoHAoYCNaI4FDARpiFNDJQEDRp6JKAIyNoB6IKALRLAQOQPRBWBgRoNPRJozBYZQBNwIADMQILDOhZQBYoQACMQILDOhZQCDIhiCBYZ0LXIJuBAAZiBBYZ0LNDoRDbqJoEbqpoDbqpoDPQYFFNEmAcYJiGHwJoNCIbLBCJpQJZYIaWgBQCDSpmBGSxoSoAA="))
+}, (x/4)+10, (y/4)+10);
 
   // queue next draw
   if (drawTimeout) clearTimeout(drawTimeout);
@@ -50,6 +96,7 @@ Bangle.setUI({
 
 // Load widgets
 Bangle.loadWidgets();
+g.clear();
 draw();
 setTimeout(Bangle.drawWidgets,0);
 }
